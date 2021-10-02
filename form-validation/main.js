5d9efe19867f06324ff3b238f752232c5d5c1500
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2021-10-02 02:09:10 -0500

    :page_facing_up: form validation 2.0

--- form-validation/main.js
+++ form-validation/main.js
@@ -1,9 +1,96 @@
 const form = document.querySelector('#form')
 const fields = document.querySelectorAll('input')
 
-const regexpCEP = /\d{5}[-\s]?\d{3}/g
-const regexpCPF = /(?:\d{3}[-.\s]?){3}\d{2}/g
-const regexpEmail = /[\w.-]+@[\w-]+\.[\w-.]+/gi
+class Validation {
+  constructor(el) {
+    this.element = el
+    this.textInput = el.value
+    this.typeInput = el.name
+
+    this.regexp = {
+      cep: /\d{5}[-\s]?\d{3}/g,
+      cpf: /(?:\d{3}[-.\s]?){3}\d{2}/g,
+      email: /[\w.-]+@[\w-]+\.[\w-.]+/gi,
+      clear: /\D/g
+    }
+
+    this.regexpFormat = {
+      cpf: [/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4'],
+      cep: [/(\d{5})(\d{3})/g, '$1-$2']
+    }
+  }
+
+  isEmpty = () => this.textInput.trim() === ''
+
+  isMatch() {
+    const match = this.textInput.match(this.regexp[this.typeInput])
+
+    if (!match || match[0] !== this.textInput) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  validPassword() {
+    let obligationsList = document.querySelectorAll('.obligations li')
+    obligationsList = [...obligationsList]
+
+    obligationsList.forEach(obligation => {
+      obligation.classList.remove('check')
+    })
+
+    for (let i in this.textInput) {
+      let character = this.textInput.charAt(i)
+      if (i >= 6 && i <= 20) {
+        obligationsList[0].classList.add('check')
+      } else {
+        obligationsList[0].classList.remove('check')
+      }
+
+      if (character == character.toLowerCase()) {
+        obligationsList[1].classList.add('check')
+      }
+      if (character == character.toUpperCase()) {
+        obligationsList[2].classList.add('check')
+      }
+      if (character.match(/\W/g)) {
+        obligationsList[3].classList.add('check')
+      }
+    }
+
+    return obligationsList.every(obligation =>
+      obligation.classList.contains('check')
+    )
+  }
+
+  verify() {
+    if (this.isEmpty()) {
+      return 'Please fill all required fields'
+    }
+
+    if (this.typeInput !== 'name' && this.typeInput !== 'password') {
+      if (!this.isMatch()) {
+        return `Invalid ${this.typeInput.toUpperCase()}`
+      } else {
+        if (this.typeInput === 'cep' || this.typeInput === 'cpf') {
+          let textInputFormat = this.textInput.replace(this.regexp.clear, '')
+          textInputFormat = textInputFormat.replace(
+            this.regexpFormat[this.typeInput][0],
+            `${this.regexpFormat[this.typeInput][1]}`
+          )
+          this.textInput = textInputFormat
+        }
+      }
+    } else if (this.typeInput === 'password') {
+      if (!this.validPassword()) {
+        return 'Please fill in the requirements'
+      }
+    }
+
+    return true
+  }
+}
 
 const renderInvalid = {
   create(el, text) {
@@ -25,114 +112,17 @@ const renderInvalid = {
   }
 }
 
-const verify = {
-  isEmpty: text => text.trim() === '',
-
-  isMatch(input, regexp) {
-    const match = input.match(regexp)
-
-    if (!match || match[0] !== input) {
-      return false
-    } else {
-      return true
-    }
-  }
-}
-
 const validInput = e => {
   e.preventDefault()
-  const types = {
-    valueInput: e.target.value,
 
-    name() {
-      if (verify.isEmpty(this.valueInput)) {
-        renderInvalid.create(e.target, 'Please fill all required fields')
-      } else {
-        renderInvalid.remove(e.target)
-      }
-    },
+  const validation = new Validation(e.target)
 
-    email() {
-      if (verify.isEmpty(this.valueInput)) {
-        renderInvalid.create(e.target, 'Please fill all required fields')
-      } else if (!verify.isMatch(this.valueInput, regexpEmail)) {
-        renderInvalid.create(e.target, 'Invalid email')
-      } else {
-        renderInvalid.remove(e.target)
-      }
-    },
-
-    cep() {
-      if (verify.isEmpty(this.valueInput)) {
-        renderInvalid.create(e.target, 'Please fill all required fields')
-      } else if (!verify.isMatch(this.valueInput, regexpCEP)) {
-        renderInvalid.create(e.target, 'Invalid CEP')
-      } else {
-        renderInvalid.remove(e.target)
-        let textInput = this.valueInput.replace(/\D/g, '')
-        textInput = textInput.replace(/(\d{5})(\d{3})/g, '$1-$2')
-        e.target.value = textInput
-      }
-    },
-
-    cpf() {
-      if (verify.isEmpty(this.valueInput)) {
-        renderInvalid.create(e.target, 'Please fill all required fields')
-      } else if (!verify.isMatch(this.valueInput, regexpCPF)) {
-        renderInvalid.create(e.target, 'Invalid CPF')
-      } else {
-        renderInvalid.remove(e.target)
-        let textInput = this.valueInput.replace(/\D/g, '')
-        textInput = textInput.replace(
-          /(\d{3})(\d{3})(\d{3})(\d{2})/g,
-          '$1.$2.$3-$4'
-        )
-        e.target.value = textInput
-      }
-    },
-
-    password() {
-      let obligationsList = document.querySelectorAll('.obligations li')
-      obligationsList = Array.from(obligationsList)
-
-      obligationsList.forEach(obligation => {
-        obligation.classList.remove('check')
-      })
-
-      for (let i in this.valueInput) {
-        let character = this.valueInput.charAt(i)
-        if (i >= 6 && i <= 20) {
-          obligationsList[0].classList.add('check')
-        } else {
-          obligationsList[0].classList.remove('check')
-        }
-
-        if (character == character.toLowerCase()) {
-          obligationsList[1].classList.add('check')
-        }
-        if (character == character.toUpperCase()) {
-          obligationsList[2].classList.add('check')
-        }
-        if (character.match(/\W/g)) {
-          obligationsList[3].classList.add('check')
-        }
-      }
-
-      const isValid = obligationsList.every(obligation =>
-        obligation.classList.contains('check')
-      )
-
-      if (verify.isEmpty(this.valueInput)) {
-        renderInvalid.create(e.target, 'Please fill all required fields')
-      } else if (!isValid) {
-        renderInvalid.create(e.target, 'Please fill in the requirements')
-      } else {
-        renderInvalid.remove(e.target)
-      }
-    }
+  if (typeof validation.verify() !== 'string') {
+    renderInvalid.remove(e.target)
+    e.target.value = validation.textInput
+  } else {
+    renderInvalid.create(e.target, validation.verify())
   }
-
-  types[e.target.name]()
 }
 
 fields.forEach(field => {
